3c3ff80227cb0194c7713ce9a952953105f863c7
Anthony Rey <anthony.rey21@gmail.com>
2017-04-18 16:02:03 +1000

    Work on Start/Stop Button.

--- SafeTraffic/ViewController.swift
+++ SafeTraffic/ViewController.swift
@@ -9,9 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var controlAnimationBtn: UIButton!
+    
+    var animationRunning: Bool = false;
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("ViewDidLoad")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +26,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTouchControlAnimationBtn(_ sender: Any) {
+        print("Touched Button")
+        if (animationRunning == false) {
+            print("---> Starting Simulation.")
+            controlAnimationBtn.setTitle("Stop", for: UIControlState.normal)
+            animationRunning = true
+        } else {
+            print("---> Stopping Simulation.")
+            controlAnimationBtn.setTitle("Start", for: UIControlState.normal)
+            animationRunning = false
+        }
+        
+    }
 
 }
 
